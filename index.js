68a3093d8987b8bd574829bdefc412becfc562eb
emersion <contact@emersion.fr>
2015-04-05 18:09:27 +0000

    Adds quadcopter schema

--- index.js
+++ index.js
@@ -8,16 +8,28 @@ var expressWs = require('express-ws')(app);
 
 var servo = servoblaster.createWriteStream();
 
+var clients = [];
+function broadcast(msg) {
+	clients.forEach(function (ws) {
+		ws.send(BSON.serialize(msg));
+	});
+}
+
 var handlers = {
 	power: function (val) {
 		val = Math.round(val * 200);
 		console.log('Power: '+val);
 		servo.write({ pin: 0, value: val });
+
+		broadcast({
+			type: 'motors-speed',
+			speed: [val/200, 0, 0, 0]
+		});
 	}
 };
 
 app.ws('/socket', function (ws, req) {
-	var sendMsg = function (msg) {
+	var send = function (msg) {
 		ws.send(BSON.serialize(msg));
 	};
 
@@ -25,12 +37,22 @@ app.ws('/socket', function (ws, req) {
 		var msg = BSON.deserialize(data);
 		
 		if (!handlers[msg.cmd]) {
-			sendMsg({ type: 'error', msg: 'Invalid command: "'+msg.cmd+'"' });
+			send({ type: 'error', msg: 'Invalid command: "'+msg.cmd+'"' });
 			return;
 		}
 
 		handlers[msg.cmd](msg.opts);
 	});
+
+	send({
+		type: 'motors-speed',
+		speed: [0, 0, 0, 0]
+	});
+
+	ws.on('close', function () {
+		clients.splice(clients.indexOf(ws), 1);
+	});
+	clients.push(ws);
 });
 
 app.use(express.static(__dirname+'/public'));
--- public/assets/main.js
+++ public/assets/main.js
@@ -1,6 +1,31 @@
 var BSON = bson().BSON;
 var ws;
 
+var percentColors = [
+    { pct: 0.0, color: { r: 0xff, g: 0x00, b: 0 } },
+    { pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
+    { pct: 1.0, color: { r: 0x00, g: 0xff, b: 0 } } ];
+
+var getColorForPercentage = function(pct) {
+    for (var i = 1; i < percentColors.length - 1; i++) {
+        if (pct < percentColors[i].pct) {
+            break;
+        }
+    }
+    var lower = percentColors[i - 1];
+    var upper = percentColors[i];
+    var range = upper.pct - lower.pct;
+    var rangePct = (pct - lower.pct) / range;
+    var pctLower = 1 - rangePct;
+    var pctUpper = rangePct;
+    var color = {
+        r: Math.floor(lower.color.r * pctLower + upper.color.r * pctUpper),
+        g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
+        b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper)
+    };
+    return color;
+}
+
 function Joystick(el, oninput) {
 	var that = this;
 
@@ -54,6 +79,22 @@ function Joystick(el, oninput) {
 	this.joystick.trigger('mouseup');
 }
 
+function QuadcopterSchema(svg) {
+	this.svg = $(svg);
+}
+QuadcopterSchema.prototype.setSpeed = function (speed) {
+	var colorOffset = -150;
+	var propellers = this.svg.find('#propellers > g');
+
+	for (var i = 0; i < speed.length; i++) {
+		var s = speed[i];
+		var color = getColorForPercentage(1 - s);
+
+		var rgb = 'rgb(' + [color.r+colorOffset, color.g+colorOffset, color.b+colorOffset].join(',') + ')';
+		$(propellers[i]).css('fill', rgb);
+	}
+};
+
 function sendCommand(cmd, opts) {
 	ws.send(BSON.serialize({
 		cmd: cmd,
@@ -66,27 +107,49 @@ function init() {
 		sendCommand('direction', pos);
 	});
 
+	var lastPower;
 	$('#power-input').on('input', function () {
-		var val = parseFloat($(this).val()) / 100;
-		sendCommand('power', val);
+		var val = parseFloat($(this).val());
+		if (lastPower === val) {
+			return;
+		}
+
+		lastPower = val;
+		sendCommand('power', val / 100);
 	});
 }
 
 $(function () {
+	var quadMotors;
+
+	// Inject SVGs
+	var svgs = $('img[src$=".svg"]');
+	SVGInjector(svgs, null, function () {
+		quadMotors = new QuadcopterSchema('#quadcopter');
+	});
+
+	// Console
 	var $console = $('#console pre');
 
 	function log(msg) {
 		$console.append(msg, '\n');
 	}
 
-
+	// Events handlers
 	var handlers = {};
 
 	handlers.error = function (event) {
 		log('<span class="error">'+event.msg+'</span>');
 	};
+	handlers.info = function (event) {
+		log('<span class="info">'+event.msg+'</span>');
+	};
 
+	handlers['motors-speed'] = function (event) {
+		quadMotors.setSpeed(event.speed);
+	};
 
+	// Init
 	log('Connecting to server...');
 
 	ws = new WebSocket('ws://'+window.location.host+'/socket');
@@ -102,6 +165,7 @@ $(function () {
 		
 		if (!handlers[msg.type]) {
 			console.error('Unsupported message type: "'+msg.type+'"');
+			return;
 		}
 
 		handlers[msg.type](msg);
