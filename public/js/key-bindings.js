e2d4048318209b04db432ed8d827a5f50afaf82e
emersion <contact@emersion.fr>
2015-06-15 23:41:46 +0200

    Adds better PID and key bindings support

--- public/js/key-bindings.js
+++ public/js/key-bindings.js
@@ -1,10 +1,30 @@
 module.exports = function (quad) {
 	window.addEventListener('keydown', function (event) {
+		var activeTag = document.activeElement.tagName.toLowerCase();
+
+		// First-class key bindings
+		// Always active
+		var handled = true;
 		switch (event.key) {
 			case 'Escape':
 			case 'Esc':
 				quad.enabled = false;
 				break;
+			default:
+				handled = false;
+		}
+		if (handled) {
+			event.preventDefault();
+			return;
+		}
+
+		// Check if an input has the focus
+		// If so, stop here
+		if (['input', 'textarea', 'select'].indexOf(activeTag) >= 0) return;
+
+		// Second-class key bindings
+		// Ignored when an input is being focused
+		switch (event.key) {
 			case 'ArrowDown':
 				quad.power -= 0.05;
 				break;
@@ -14,6 +34,7 @@ module.exports = function (quad) {
 			default:
 				return;
 		}
+
 		event.preventDefault();
 	});
 };
