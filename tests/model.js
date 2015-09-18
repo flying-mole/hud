5aef4c26b56a4b4aea756378f0f6487bf82b7725
emersion <contact@emersion.fr>
2015-09-18 17:12:35 +0200

    Makes Model simplier in tests

--- tests/model.js
+++ tests/model.js
@@ -4,15 +4,12 @@ class Model {
 	constructor(config) {
 		this.config = config; // Quadcopter config
 
-		this.t = 0; // Current time, in milliseconds
-	}
-
-	set motorsSpeed(speeds) {
-		// TODO: do something with motor speeds
+		this.t = 0;
+		this.motorsSpeed = null;
 	}
 
 	get orientation() {
-		// TODO: compute orientation
+		// TODO: compute orientation based on this.t and this.motorsSpeed
 
 		return {
 			accel: { x: 0, y: 0, z: 0 },
