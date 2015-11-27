6b0a5d09e10905684a258efdf85856c211f8b449
emersion <contact@emersion.fr>
2015-11-27 14:43:46 +0100

    Adds comments in config

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
 		"diagonalLength": 45, // motor-motor distance in cm
 		"boxHeight": 8, // distance between the axis and the center of gravity of the box in cm
 		"boxMass": 650, // mass of the box in grams
-		"linearRegressionForce" : 0.13 // Regression linéaire de la force de la seconde expèrience, 
+		"linearRegressionForce" : 0.13 // Regression linéaire de la force de la seconde expèrience,
 	},
 	"mpu6050": {
 		"device": 1, // RaspberryPi I2C device (0 or 1)
@@ -15,19 +15,19 @@ module.exports = {
 	},
 	"servos": {
 		"pins": [0, 1, 2, 3], // ServoBlaster motor outputs
-		"range": [85, 140, 185], // Output period range
+		"range": [85, 140, 185], // Motor commands range (minimum, safe maximum, unsafe maximum)
 		"initPeriod": 100, // ESC initialization period
 		"massToPeriod": [91.344803622, 0.0676439999, 0.0000557704991374891] // coeffs for cst, x and x²
 	},
 	"broadcastInterval": {
-		"osStatus": 10,
-		"orientation": 0.2
+		"osStatus": 10, // How frequently is the OS status broadcast?
+		"orientation": 0.2 // How frequently is the quad orientation broadcast?
 	},
 	"controller": {
-		"interval": 100,
-		"updater": "dummy"
+		"interval": 100, // Call the updater each XX ms
+		"updater": "dummy" // Default updater to use
 	},
-	"updaters": {
+	"updaters": { // Updaters config
 		"dummy": {
 			"targetFactor": 0.2
 		},
