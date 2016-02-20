1f0ee2967c194b6d7c16f9baa5fa34c406a16348
emersion <contact@emersion.fr>
2016-02-20 10:26:51 +0100

    Finished camera profile widget inputs

--- public/js/widget/camera-profile.js
+++ public/js/widget/camera-profile.js
@@ -41,6 +41,24 @@ var meterings = [
 	'matrix'
 ];
 
+var drcs = [
+	'off',
+	'low',
+	'medium',
+	'high'
+];
+
+var modes = [
+	'Auto',
+	'1920x1080, 16:9, 1-30fps',
+	'2592x1944, 4:3, 1-15fps',
+	'2592x1944, 4:3, 0.1666-1fps',
+	'1296x972, 4:3, 1-42fps',
+	'1296x730, 16:9, 1-49fps',
+	'640x480, 4:3, 42.1-60fps',
+	'640x480, 4:3, 60.1-90fps'
+];
+
 function CameraProfile() {
 	return hg.state({
 		config: hg.value(),
@@ -123,7 +141,40 @@ CameraProfile.render = function (state) {
 			name: 'metering'
 		}, meterings.map(function (item) {
 			return h('option', { value: item, selected: (state.config.metering === item) }, item || 'default');
-		})))
+		}))),
+		formGroup(h('abbr', { title: 'Dynamic Range Compression' }, 'DRC'), h('select.form-control', {
+			name: 'drc'
+		}, drcs.map(function (item) {
+			return h('option', { selected: (state.config.drc === item) }, item);
+		}))),
+		formGroup('Mode', h('select.form-control', {
+			name: 'mode'
+		}, modes.map(function (title, i) {
+			return h('option', { value: i, selected: (state.config.mode === i) }, title);
+		}))),
+		formGroup('Size', [
+			'Width: ', h('input.form-control', {
+				type: 'number',
+				name: 'width',
+				value: state.config.width
+			}), 'px',
+			h('br'),
+			'Height: ', h('input.form-control', {
+				type: 'number',
+				name: 'height',
+				value: state.config.height
+			}), 'px'
+		]),
+		formGroup(h('abbr', { title: 'Frames Per Second' }, 'FPS'), h('input.form-control', {
+			type: 'number',
+			name: 'framerate',
+			value: state.config.framerate
+		})),
+		h('.form-group', h('.col-sm-offset-2.col-sm-10', [
+			h('button.btn.btn-primary', { type: 'submit' }, 'Submit'),
+			' ',
+			h('button.btn.btn-default', { type: 'reset' }, 'Reset')
+		]))
 	]);
 };
 
